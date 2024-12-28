febd8e99be84ea86ff57d0e311ac56b75cbe9c80
devB <21kjh@BOOK-GDUMOSJ3R0>
2024-12-29 00:16:21 +0900

    This is master branch

--- Git/src/Gitpack/B.java
+++ Git/src/Gitpack/B.java
@@ -5,6 +5,7 @@ public class B {
 	public static void main(String[] args) {
 		System.out.println("This is B");
 		System.out.println("This is updated B");
+		System.out.println("This is master branch2");
 
 	}
 
