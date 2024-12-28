8580c78c9f9c6dbf1a7579b2c50daafe5fab4a9b
devB <21kjh@BOOK-GDUMOSJ3R0>
2024-12-29 00:06:42 +0900

    This is devB

--- Git/src/Gitpack/A.java
+++ Git/src/Gitpack/A.java
@@ -5,6 +5,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("This is A");
 		System.out.println("This is updated A");
+		System.out.println("This is devB");
 
 	}
 
